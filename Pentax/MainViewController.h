21e3619539827eeb00e0ee710628f5d6ec1ef214
coore <coore@coores-Mac-mini.local>
2017-03-08 10:22:36 +0800

    initial commit

---
+++ Pentax/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Pentax
+//
+//  Created by coore on 3/7/17.
+//  Copyright © 2017 PentaxOrganizationName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ Pods/Target Support Files/Pods-Pentax/Pods-Pentax-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pentax : NSObject
+@end
+@implementation PodsDummy_Pods_Pentax
+@end
